14697f49682f56e9b6671ffa00c128d96a04e255
Janosik232 <janbialek82@gmail.com>
2025-11-13 16:54:35 +0100

    zadaniedom.php

--- zadaniedom.php
+++ zadaniedom.php
@@ -1,26 +1,26 @@
 <?php
 
-$dane_bazowe = array(7, 3, 1, 6, 9, 5, 4, 10, 2, 2);
+$dane = array(7, 3, 1, 6, 9, 5, 4, 10, 2, 2);
 
-function odczytaj_komorke($tablica, $numer_komorki) {
-    return $tablica[$numer_komorki];
+function odczytaj($tab, $numer) {
+    return $tab[$numer];
 }
 
-echo "2.1<br>" . odczytaj_komorke($dane_bazowe, 4) . "<br>";
-print_r($dane_bazowe);
+echo "2.1<br>" . odczytaj($dane, 4) . "<br>";
+print_r($dane);
 echo "<br>";
 
-function zmien_komorke($tablica, $numer_komorki, $nowa_wartosc) {
-    $tablica[$numer_komorki] = $nowa_wartosc;
-    return $tablica;
+function zmien($tab, $numer, $nowa_val) {
+    $tab[$numer] = $nowa_val;
+    return $tab;
 }
 
-$dane_bazowe = zmien_komorke($dane_bazowe, 6, 12);
+$dane = zmien($dane, 6, 12);
 echo "2.2";
-print_r($dane_bazowe);
+print_r($dane);
 echo "<br>";
 
-function skopiuj_tablice($zrodlo) {
+function skopiuj_tab($zrodlo) {
     $kopia = array();
     for ($i = 0; $i < count($zrodlo); $i++) {
         $kopia[$i] = $zrodlo[$i];
@@ -28,25 +28,25 @@ function skopiuj_tablice($zrodlo) {
     return $kopia;
 }
 
-$tablica_kopia = skopiuj_tablice($dane_bazowe);
+$tab_kop = skopiuj_tab($dane);
 echo "3.1";
-print_r($tablica_kopia);
+print_r($tab_kop);
 echo "<br>";
 
-function sumuj_indeksy($tablica_a, $tablica_b) {
+function suma($tab_a, $tab_b) {
     $suma = array();
-    for ($i = 0; $i < count($tablica_a); $i++) {
-        $suma[$i] = $tablica_a[$i] + $tablica_b[$i];
+    for ($i = 0; $i < count($tab_a); $i++) {
+        $suma[$i] = $tab_a[$i] + $tab_b[$i];
     }
     return $suma;
 }
 
-$tablica_sum = sumuj_indeksy($dane_bazowe, $tablica_kopia);
+$tab_sum = suma($dane, $tab_kop);
 echo "3.2";
-print_r($tablica_sum);
+print_r($tab_sum);
 echo "<br>";
 
-function odwroc_kolejnosc_i_przepisz($zrodlo) {
+function odwrotna_kolejnosc($zrodlo) {
     $odwrocona = array();
     $dlugosc = count($zrodlo);
     for ($i = 0; $i < $dlugosc; $i++) {
@@ -55,35 +55,35 @@ function odwroc_kolejnosc_i_przepisz($zrodlo) {
     return $odwrocona;
 }
 
-$tablica_kopia = odwroc_kolejnosc_i_przepisz($dane_bazowe);
+$tab_kop = odwrotna_kolejnosc($dane);
 echo "3.3";
-print_r($tablica_kopia);
+print_r($tab_kop);
 echo "<br>";
 
-function oblicz_srednia($tablica) {
+function oblicz_sr($tab) {
     $suma = 0;
-    for ($i = 0; $i < count($tablica); $i++) {
-        $suma += $tablica[$i];
+    for ($i = 0; $i < count($tab); $i++) {
+        $suma += $tab[$i];
     }
-    return $suma / count($tablica);
+    return $suma / count($tab);
 }
 
-echo "4.1" . oblicz_srednia($dane_bazowe) . "<br>";
-print_r($dane_bazowe);
+echo "4.1" . oblicz_sr($dane) . "<br>";
+print_r($dane);
 echo "<br>";
 
-function znajdz_maksimum($tablica) {
-    $maksimum = $tablica[0];
-    for ($i = 1; $i < count($tablica); $i++) {
-        if ($tablica[$i] > $maksimum) {
-            $maksimum = $tablica[$i];
+function znajdz_max($tab) {
+    $max = $tab[0];
+    for ($i = 1; $i < count($tab); $i++) {
+        if ($tab[$i] > $max) {
+            $max = $tab[$i];
         }
     }
-    return $maksimum;
+    return $max;
 }
 
-echo "4.2<br>" . znajdz_maksimum($tablica_sum) . "<br>";
-print_r($tablica_sum);
+echo "4.2<br>" . znajdz_max($tab_sum) . "<br>";
+print_r($tab_sum);
 echo "<br>";
 
 ?>
